82112fa4e0d9caffd22808a45d440db8c5169c9d
sylar0012 <kmc42692@naver.com>
2022-11-09 16:57:31 +0900

    airbub : 기본 뼈대 완성 및 해더 뼈대 완성

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_airbnb/pages/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+// 최초의 메인 페이지
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
